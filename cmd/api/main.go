19dc085c00c83290ef2d24082f811a716fbc2d42
Raja Roy <raja.r@kinaracapital.com>
2024-10-22 23:29:11 +0530

    basic structure with db set up and docker
    - Add hot reloading
    - Env setup
    - docker setup
    - migration scripts
    - health check is ready

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,14 +1,46 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/Rajaxroy/social/internal/db"
+	"github.com/Rajaxroy/social/internal/env"
+	"github.com/Rajaxroy/social/internal/store"
+)
 
 func main() {
 	cfg := config{
-		addr: ":8080",
+		addr: env.GetString("ADDRESS", ":8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/socialnetwork?sslmode=disable"),
+			maxOpenConns: 30,
+			maxIdleConns: 30,
+			maxIdleTime:  "15m",
+		},
 	}
+
+	db, err := db.New(
+		cfg.db.addr,
+		cfg.db.maxOpenConns,
+		cfg.db.maxIdleConns,
+		cfg.db.maxIdleTime,
+	)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer db.Close()
+
+	log.Print("DataBase connection pool established...")
+
+	store := store.NewStorage(db)
+
 	app := &application{
 		config: cfg,
+		store:  store,
 	}
+
 	mux := app.mount()
 	log.Fatal(app.run(mux))
 }
